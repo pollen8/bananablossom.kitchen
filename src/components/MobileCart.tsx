8475cc7e84e0b6be768d7f37199b3c8b15a2b7ca
Rob Clayburn <rob@infosum.com>
2020-02-27 00:23:56 +0000

    add box shadow to mobile footer

--- src/components/MobileCart.tsx
+++ src/components/MobileCart.tsx
@@ -21,6 +21,7 @@ const Footer = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  box-shadow: 5px 7px 14px 10px rgba(60,66,87,0.12),0 3px 6px 0 rgba(0,0,0,0.12)
 `;
 
 const MobileCart: FC = () => {
