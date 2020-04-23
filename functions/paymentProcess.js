0d9d3275f749cc7d5916f76e35c8b38e9944219b
Rob Clayburn <rob@infosum.com>
2020-04-23 10:11:50 +0100

    curreny!

--- functions/paymentProcess.js
+++ functions/paymentProcess.js
@@ -17,6 +17,7 @@ exports.handler = async (event) => {
   try {
     const intent = await stripe.paymentIntents.create({
       amount: data.amount,
+      currency: 'gbp',
       receipt_email: data.email,
       metadata: {
         order: data.order.join('; '),
