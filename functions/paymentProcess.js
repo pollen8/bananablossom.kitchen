f197239ca0f9f88f293f5a41d88c2bd20c499894
Rob Clayburn <rob@infosum.com>
2020-05-01 18:32:56 +0100

    add logging to payment process

--- functions/paymentProcess.js
+++ functions/paymentProcess.js
@@ -1,5 +1,12 @@
 const k = process.env.FUNCTIONAL_TESTS_URL ? 'STRIPE_SECRET_KEY_TEST' : 'STRIPE_SECRET_KEY';
 const stripe = require('stripe')(process.env[k]);
+const faunadb = require('faunadb');
+
+/* configure faunaDB Client with our secret */
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET
+})
 
 exports.handler = async (event) => {
 
@@ -11,10 +18,13 @@ exports.handler = async (event) => {
     };
   }
 
-  // Parse the body contents into an object.
-  const data = JSON.parse(event.body);
 
   try {
+    // Parse the body contents into an object.
+    const data = JSON.parse(event.body);
+
+    response = await client.query(q.Create(q.Ref("classes/payment_intents"), { type: 'start', data }));
+
     const intent = await stripe.paymentIntents.create({
       amount: data.amount,
       currency: 'gbp',
@@ -23,13 +33,14 @@ exports.handler = async (event) => {
         order: data.order.join('; '),
       },
     });
-
+    response = await client.query(q.Create(q.Ref("classes/payment_intents"), { type: 'ok', data: intent }));
     return {
       statusCode: 200,
       body: JSON.stringify({ statusCode: 200, client_secret: intent.client_secret, k })
     };
 
   } catch (e) {
+    response = await client.query(q.Create(q.Ref("classes/payment_intents"), { type: 'error', e }));
     return {
       statusCode: 200,
       body: JSON.stringify({ statusCode: e.statusCode, message: e.raw.message, details: e.raw }),
