b66b76816afbafeefcd873907c10480e62073a9d
Rob Clayburn <rob@infosum.com>
2020-05-05 02:44:48 +0100

    decrease card shadow

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,7 @@ import React, {
 import styled from 'styled-components';
 
 const StyledCard = styled.div`
-  box-shadow: 0 7px 14px 0 rgba(60,66,87, 0.12), 0 3px 6px 0 rgba(0,0,0, 0.12);
+  box-shadow: 0 3px 6px 0 rgba(0,0,0, 0.12);
   border-radius: 0.3rem;
   background-color: rgb(255, 255, 255);
 `;
