0f775334948a561441e3bccc5a8aada6c9f1d1d5
Rob Clayburn <rob@infosum.com>
2020-06-21 17:42:24 +0100

    change order time

--- src/components/OrderHelp.tsx
+++ src/components/OrderHelp.tsx
@@ -11,7 +11,7 @@ const OrderHelp: FC = () => {
           Dishes may contain allergens. If you have any dietary requirements, please let us know when you place your order.
         </p>
         <p>
-          All our dishes are freshly made to order, please orders by 17h30 for next day collection. </p>
+          All our dishes are freshly made to order, please orders by 11:00pm for next day collection. </p>
         <p>
           The “Pho bo” beef broth is only available on Thursdays by order the previous day.
         </p>
