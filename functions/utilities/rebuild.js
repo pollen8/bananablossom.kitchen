e6700e5d3b8bc168195c4a3f2ad5e71f08875840
Rob Clayburn <rob@infosum.com>
2020-07-28 21:49:43 +0100

    wait rebuild

--- functions/utilities/rebuild.js
+++ functions/utilities/rebuild.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
 
 exports.rebuildSite = async () => {
-  axios.post('https://api.netlify.com/build_hooks/5e4207320a25ca7d73f7b00e');
+  await axios.post('https://api.netlify.com/build_hooks/5e4207320a25ca7d73f7b00e');
 }
