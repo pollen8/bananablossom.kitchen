acd8e03d8254d5096efa244f58fb227f133f1e57
Rob Clayburn <rob@infosum.com>
2020-09-07 22:58:07 +0100

    add auto generated product pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+const path = require(`path`)
 
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions;
@@ -23,4 +24,54 @@ exports.createSchemaCustomization = ({ actions }) => {
         message: String
     }
   `)
+}
+
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  // **Note:** The graphql function call returns a Promise
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  const result = await graphql(`
+    query {
+      allFaunaProduct {
+        nodes {
+          _id
+          name
+          skus {
+            id
+            name
+            glutenFree
+            image
+            nuts
+            price
+            unavailable
+            vegan
+            vegetarian
+          }
+          availableDays
+          course
+          description
+          id
+          price
+        }
+      }
+    }
+  `)
+
+  result.data.allFaunaProduct.nodes.forEach((node) => {
+    node.skus.forEach((sku) => {
+      createPage({
+        path: node.name + '/' + sku.name,
+        component: path.resolve(`./src/templates/product.tsx`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          product: node,
+          sku: sku,
+        },
+      })
+    })
+
+  })
 }
---
+++ src/templates/product.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Cart from '../components/Cart';
+// import Layout from '../components/layout';
+import Layout from '../components/mealLayout';
+import MealListItemDetails from '../components/MealListItemDetails';
+
+const ThisLayout = styled.div`
+@media (min-width: 640px){
+  margin: 10px;
+}
+`;
+
+export default function BlogPost({ pageContext }) {
+  return (
+    <Layout>
+      <ThisLayout>
+        <div>
+          <MealListItemDetails product={pageContext.product} />
+        </div>
+      </ThisLayout>
+    </Layout>
+  );
+}
