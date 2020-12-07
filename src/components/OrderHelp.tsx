98f6ae77122086a95445f8464e89b1e2e8f8abaa
Rob Clayburn <rob@infosum.com>
2020-12-07 21:45:41 +0000

    remove delivery amount message

--- src/components/OrderHelp.tsx
+++ src/components/OrderHelp.tsx
@@ -6,7 +6,7 @@ const OrderHelp: FC = () => {
   return (
     <>
       <div id="order-help" style={{ marginTop: '1rem', fontSize: '0.7rem' }}>
-        <p>Delivery around Basingstoke for orders over £{checkoutConfig.deliveryFreeFrom}.00</p>
+        {/* <p>Delivery around Basingstoke for orders over £{checkoutConfig.deliveryFreeFrom}.00</p> */}
         <p>
           Dishes may contain allergens. If you have any dietary requirements, please let us know when you place your order.
         </p>
