971bd129de038389bb3f78af8199b73da6cdeb7f
Rob Clayburn <rob@infosum.com>
2021-04-05 21:34:12 +0100

    remove delivery info from thursday menu and set pickup time to 1pm sat

--- src/pages/thursday-take-away.tsx
+++ src/pages/thursday-take-away.tsx
@@ -11,7 +11,7 @@ export default () => {
       <CardBody>
         <div id="pre-order">
           <Alert color="info">
-            Our kitchen will be opened for collection and delivery on Thursdays from 5:30pm to 8pm.<br />
+            Our kitchen will be opened for collection on Thursdays from 5:30pm to 8pm.<br />
 Order accepted up until 12pm on Wednesday.
 </Alert>
           <br />
